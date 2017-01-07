dbb572259bc5687a7a471c4ba41bfec8fb461846
Karl Odie <karlodie@gmail.com>
2017-01-07 16:18:07 +0000

    Stop widget being overridden if set to a multiwidget containing a markdownx widget (#38)

--- markdownx/fields.py
+++ markdownx/fields.py
@@ -11,5 +11,11 @@ class MarkdownxFormField(forms.CharField):
     def __init__(self, *args, **kwargs):
         super(MarkdownxFormField, self).__init__(*args, **kwargs)
 
-        if not issubclass(self.widget.__class__, MarkdownxWidget):
+        if issubclass(self.widget.__class__, forms.widgets.MultiWidget):
+            if not any([
+                issubclass(x.__class__, MarkdownxWidget)
+                for x in self.widget.widgets
+            ]):
+                self.widget = MarkdownxWidget()
+        elif not issubclass(self.widget.__class__, MarkdownxWidget):
             self.widget = MarkdownxWidget()
