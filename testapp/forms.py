29f327c45abace8062ac7f20f5d9b80dac678ce6
adi <aaadeji@gmail.com>
2017-01-16 21:10:09 +0100

    testapp update

--- testapp/forms.py
+++ testapp/forms.py
@@ -1,7 +1,8 @@
 from django import forms
 
 from markdownx.fields import MarkdownxFormField
+from markdownx.widgets import MarkdownxWidget
 
 class MyForm(forms.Form):
-    markdownx_form_field1 = MarkdownxFormField()
-    markdownx_form_field2 = MarkdownxFormField()
+    markdownx_form_field1 = MarkdownxFormField(widget=MarkdownxWidget(attrs={'class':'custom-class-markdownx_form_field1'}))
+    markdownx_form_field2 = MarkdownxFormField(widget=MarkdownxWidget(attrs={'class':'custom-class-markdownx_form_field2'}))
