fa820cfe8bd1eebbe833ba63a6cb1fe74d3b01cb
adi <aaadeji@gmail.com>
2017-03-10 11:08:57 +0100

    setup.py update

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def get_requirements():
 
 setup(
     name='django-markdownx',
-    version='1.7.2',
+    version='1.8.1',
     packages=find_packages(),
     include_package_data=True,
     description='django-markdownx is a Markdown editor built for Django.',
