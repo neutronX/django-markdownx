38649c964a56edc07bbb138ab330c6a562113f26
Pouria Hadjibagheri <p.h.bagheri@gmail.com>
2017-03-12 20:29:15 +0000

    Separated exceptions, and added a JavaScript monitoring for SVG files.

--- markdownx/settings.py
+++ markdownx/settings.py
@@ -1,66 +1,76 @@
 # Django library.
 from django.conf import settings
 from django.utils.translation import ugettext_lazy as _
+from django.core.exceptions import ImproperlyConfigured
 
 
 # Constants
 # ------------------------------------------------------------------
 
-__FIFTY_MEGABYTES = 50 * 1024 * 1024
-__VALID_CONTENT_TYPES = 'image/jpeg', 'image/png', 'image/svg+xml'
+FIFTY_MEGABYTES = 50 * 1024 * 1024
+VALID_CONTENT_TYPES = 'image/jpeg', 'image/png', 'image/svg+xml'
+NINETY_DPI = 90
+IM_WIDTH = 500
+IM_HEIGHT = 500
 
 # ------------------------------------------------------------------
 
 
-def _from_settings(var, default):
+def _mdx(var, default):
     """
     Adds "MARXDOWX_" to the requested variable and retrieves its value
-     from settings or returns the default.
+    from settings or returns the default.
 
     :param var: Variable to be retrieved.
     :type var: str
     :param default: Default value if the variable is not defined.
     :return: Value corresponding to 'var'.
     """
-    return getattr(settings, 'MARKDOWNX_' + var, default)
+    try:
+        return getattr(settings, 'MARKDOWNX_' + var, default)
+    except ImproperlyConfigured:
+        # To handle the auto-generation of documentations.
+        return default
 
 
 # Markdownify
-# ------------
-MARKDOWNX_MARKDOWNIFY_FUNCTION = _from_settings('MARKDOWNIFY_FUNCTION', 'markdownx.utils.markdownify')
+# --------------------
+MARKDOWNX_MARKDOWNIFY_FUNCTION = _mdx('MARKDOWNIFY_FUNCTION', 'markdownx.utils.markdownify')
 
 
 # Markdown extensions
 # --------------------
-MARKDOWNX_MARKDOWN_EXTENSIONS = _from_settings('MARKDOWN_EXTENSIONS', list())
+MARKDOWNX_MARKDOWN_EXTENSIONS = _mdx('MARKDOWN_EXTENSIONS', list())
 
-MARKDOWNX_MARKDOWN_EXTENSION_CONFIGS = _from_settings('MARKDOWN_EXTENSION_CONFIGS', dict())
+MARKDOWNX_MARKDOWN_EXTENSION_CONFIGS = _mdx('MARKDOWN_EXTENSION_CONFIGS', dict())
 
 
 # Markdown urls
-# --------------
-MARKDOWNX_URLS_PATH = _from_settings('URLS_PATH', '/markdownx/markdownify/')
+# --------------------
+MARKDOWNX_URLS_PATH = _mdx('URLS_PATH', '/markdownx/markdownify/')
 
-MARKDOWNX_UPLOAD_URLS_PATH = _from_settings('UPLOAD_URLS_PATH', '/markdownx/upload/')
+MARKDOWNX_UPLOAD_URLS_PATH = _mdx('UPLOAD_URLS_PATH', '/markdownx/upload/')
 
 
 # Media path
-# -----------
-MARKDOWNX_MEDIA_PATH = _from_settings('MEDIA_PATH', 'markdownx/')
+#  --------------------
+MARKDOWNX_MEDIA_PATH = _mdx('MEDIA_PATH', 'markdownx/')
 
 
 # Image
-# ------
-MARKDOWNX_UPLOAD_MAX_SIZE = _from_settings('UPLOAD_MAX_SIZE', __FIFTY_MEGABYTES)
+# --------------------
+MARKDOWNX_UPLOAD_MAX_SIZE = _mdx('UPLOAD_MAX_SIZE', FIFTY_MEGABYTES)
 
-MARKDOWNX_UPLOAD_CONTENT_TYPES = _from_settings('UPLOAD_CONTENT_TYPES', __VALID_CONTENT_TYPES)
+MARKDOWNX_UPLOAD_CONTENT_TYPES = _mdx('UPLOAD_CONTENT_TYPES', VALID_CONTENT_TYPES)
 
-MARKDOWNX_IMAGE_MAX_SIZE = _from_settings('IMAGE_MAX_SIZE', dict(size=(500, 500), quality=90))
+MARKDOWNX_IMAGE_MAX_SIZE = _mdx('IMAGE_MAX_SIZE', dict(size=(IM_WIDTH, IM_HEIGHT), quality=NINETY_DPI))
+
+MARKDOWNX_SVG_JAVASCRIPT_PROTECTION = True
 
 
 # Editor
-# -------
-MARKDOWNX_EDITOR_RESIZABLE = _from_settings('EDITOR_RESIZABLE', True)
+# --------------------
+MARKDOWNX_EDITOR_RESIZABLE = _mdx('EDITOR_RESIZABLE', True)
 
 
 # ------------------------------------------------
@@ -68,11 +78,15 @@ MARKDOWNX_EDITOR_RESIZABLE = _from_settings('EDITOR_RESIZABLE', True)
 # ------------------------------------------------
 # This is not called using `_from_settings` as
 # it does not need "_MARKDOWNX" prefix.
-LANGUAGES = getattr(
-    settings,
-    'LANGUAGES',
-    (
-        ('en', _('English')),
-        ('pl', _('Polish')),
+try:
+    LANGUAGES = getattr(
+        settings,
+        'LANGUAGES',
+        (
+            ('en', _('English')),
+            ('pl', _('Polish')),
+        )
     )
-)
+except ImproperlyConfigured:
+    # To handle the auto-generation of documentations.
+    pass
