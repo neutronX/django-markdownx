3544aead9b489af34f59e2cbb9a7116214c6fcb0
Pouria Hadjibagheri <p.h.bagheri@gmail.com>
2017-03-12 20:39:45 +0000

    New version strategy: updates only needed in __init__, docs and setup files will then automatically follow.

--- setup.py
+++ setup.py
@@ -1,23 +1,40 @@
 """
-Automatic setup protocols.
+Installation
+============
+Using PIP
+---------
+Django MarkdownX may be installed directly using Python Package Index (PyPi):
 
-Django MarkdownX may be installed directly using Python Package Index (PyPi); however, should you wish to download and
-install it using the source code, you can do as follows:
+.. code-block:: bash
 
-**Note**: Make sure you have activated your virtual environment if you're using one.
+    python3 -m pip install django-markdownx
 
-* We start off by downloading the source code from GitHub:
-.. code:: bash
-   https://github.com/adi-/django-markdownx.git
 
-* navigate to the downloaded directory:
-.. code:: bash
-   cd django-markdownx/
+From the source
+---------------
+Should you wish to download and install it using the source code, you can do as follows:
 
-* Install the package. You can replace `python3` with `python` or any other supported version of Python (2.7, 3.4+) if
+Note
+    Make sure you have activated your virtual environment if you're using one.
+
+We start off by downloading the source code from GitHub and navigate to the downloaded directory:
+
+.. code-block:: bash
+
+     git clone https://github.com/adi-/django-markdownx.git
+     cd django-markdownx/
+
+
+Install the package. You can replace ``python3`` with ``python`` or any of |Supported_versions_of_Python| if
 you have multiple versions installed on your machine:
-.. code:: bash
+
+.. code-block:: bash
+
    python3 setup.py install
+
+
+.. |Supported_versions_of_Python| image:: https://img.shields.io/pypi/pyversions/django-markdownx.svg
+
 """
 
 from setuptools import setup, find_packages
