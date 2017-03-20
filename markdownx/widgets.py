dbcdf1b5f62e6c21339e7b34ee2599b8fa386794
Aymeric Augustin <aymeric.augustin@m4x.org>
2017-03-20 10:35:06 +0100

    Fix #50 -- Native template-based widget rendering.
    On Django >= 1.11.

--- markdownx/widgets.py
+++ markdownx/widgets.py
@@ -1,3 +1,4 @@
+import django
 from django import forms
 from django.template.loader import get_template
 from django.contrib.admin import widgets
@@ -11,25 +12,43 @@ from .settings import (
 
 class MarkdownxWidget(forms.Textarea):
 
-    def render(self, name, value, attrs=None):
-        attrs = self.build_attrs(attrs, name=name)
+    if django.VERSION[:2] >= (1, 11):
 
+        template_name = 'markdownx/widget2.html'
+
+        def get_context(self, name, value, attrs=None):
+            if attrs is None:
+                attrs = {}
+
+            self.add_markdownx_attrs(attrs)
+
+            return super(MarkdownxWidget, self).get_context(name, value, attrs)
+
+    else:
+
+        def render(self, name, value, attrs=None):
+            attrs = self.build_attrs(attrs, name=name)
+
+            self.add_markdownx_attrs(attrs)
+
+            widget = super(MarkdownxWidget, self).render(name, value, attrs)
+
+            template = get_template('markdownx/widget.html')
+
+            return template.render({
+                'markdownx_editor': widget,
+            })
+
+    @staticmethod
+    def add_markdownx_attrs(attrs):
         if 'class' in attrs:
             attrs['class'] += ' markdownx-editor'
         else:
-            attrs.update({'class':'markdownx-editor'})
-
+            attrs['class'] = 'markdownx-editor'
         attrs['data-markdownx-editor-resizable'] = MARKDOWNX_EDITOR_RESIZABLE
         attrs['data-markdownx-urls-path'] = MARKDOWNX_URLS_PATH
         attrs['data-markdownx-upload-urls-path'] = MARKDOWNX_UPLOAD_URLS_PATH
 
-        widget = super(MarkdownxWidget, self).render(name, value, attrs)
-
-        template = get_template('markdownx/widget.html')
-
-        return template.render({
-            'markdownx_editor': widget,
-        })
 
     class Media:
         js = (
