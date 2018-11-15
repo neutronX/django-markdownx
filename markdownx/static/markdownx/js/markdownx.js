755001501791c6a954e0e33fc1c224ae66b5af97
adi <aaadeji@gmail.com>
2018-11-15 21:20:13 +0100

    Closes #142: Missing compiled static files

--- markdownx/static/markdownx/js/markdownx.js
+++ markdownx/static/markdownx/js/markdownx.js
@@ -652,8 +652,12 @@ exports.mountEvents = mountEvents;
 function preparePostData(data, csrf) {
     if (csrf === void 0) { csrf = true; }
     var form = new FormData();
-    if (csrf)
-        form.append("csrfmiddlewaretoken", getCookie('csrftoken'));
+    if (csrf) {
+        var csrfToken = getCookie('csrftoken');
+        if (!csrfToken)
+            csrfToken = document.querySelector("input[name='csrfmiddlewaretoken']").value;
+        form.append("csrfmiddlewaretoken", csrfToken);
+    }
     Object.keys(data).map(function (key) { return form.append(key, data[key]); });
     return form;
 }
