a33b157cccd95d2ffd83f642796ff21c63e71fd9
Jirka Schaefer <jirka@tschitschereengreen.com>
2020-12-12 17:53:22 +0100

    bump 3.0.2

--- markdownx/__init__.py
+++ markdownx/__init__.py
@@ -61,17 +61,18 @@ Key features
 # ~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=
 # Documentations
 # ~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-
-__credits__ = 'Adi, Pouria Hadjibagheri'
-__copyright__ = 'Copyright 2017'
-__license__ = 'BSD'
-__maintainer__ = 'Adi, Pouria Hadjibagheri'
-__url__ = 'https://github.com/neutronX/django-markdownx'
-__version__ = '3.0.1'
-__description__ = 'A comprehensive Markdown editor built for Django.'
+__credits__ = "Adi, Pouria Hadjibagheri"
+__copyright__ = "Copyright 2017"
+__license__ = "BSD"
+__maintainer__ = "Adi, Pouria Hadjibagheri"
+__url__ = "https://github.com/neutronX/django-markdownx"
+__version__ = "3.0.2"
+__description__ = "A comprehensive Markdown editor built for Django."
 # ~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     raise RuntimeError(
-        'MarkdownX is a Django plugin. It is not meant to be run as an stand-alone module.'
+        "MarkdownX is a Django plugin. It is not meant to be run as an stand-alone"
+        " module."
     )
