675b1700349fcd5107de6be452c1b98364310a10
Pouria Hadjibagheri <p.h.bagheri@gmail.com>
2021-12-27 11:31:55 +0000

    integrates new Django URL system w/ backward compatibility

--- testapp/urls.py
+++ testapp/urls.py
@@ -1,5 +1,10 @@
 from django.conf import settings
-from django.conf.urls import include, url
+
+try:
+    from django.conf.urls import include, url
+except ImportError:
+    from django.urls import include, re_path as url
+
 from django.conf.urls.static import static
 from django.contrib import admin
 
@@ -10,4 +15,5 @@ urlpatterns = [
     url(r'^$', TestFormView.as_view(), name='form_view'),
     url(r'^markdownx/', include('markdownx.urls')),
     url(r'^admin/', admin.site.urls),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    *static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
