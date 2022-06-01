5752c278c1e823691006d61ee62f1c31361253f0
NielkS <nielk.salocin@gmail.com>
2022-06-01 11:13:56 +0200

    [Django 4.0] request.is_ajax is deprecated since 3.1 and removed in 4.0
    Django reference: https://docs.djangoproject.com/en/4.0/releases/3.1/#id2

--- markdownx/views.py
+++ markdownx/views.py
@@ -45,7 +45,7 @@ class ImageUploadView(BaseFormView):
                  and the default response for HTTP requests.
         :rtype: django.http.JsonResponse, django.http.HttpResponse
         """
-        if self.request.is_ajax():
+        if self.request.accepts("application/json"):
             return JsonResponse(form.errors, status=400)
 
         response = super(ImageUploadView, self).form_invalid(form)
