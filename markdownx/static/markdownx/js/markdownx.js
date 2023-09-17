bcfb32f2b7f8ba7d90437b9392d6b4d9e635e737
Chris Laplante <mostthingsweb@gmail.com>
2023-09-17 14:02:42 -0400

    add bundle

--- markdownx/static/markdownx/js/markdownx.js
+++ markdownx/static/markdownx/js/markdownx.js
@@ -1,4 +1,4 @@
-(function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
+(function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.markdownx = f()}})(function(){var define,module,exports;return (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
 /**
  * **Markdownx**
  *
@@ -490,7 +490,8 @@ var MarkdownX = function (parent, editor, preview) {
         );
         xhr.success = function (response) {
             properties.preview.innerHTML = response;
-            properties.editor = updateHeight(properties.editor);
+            properties.editor = properties._editorIsResizable ?
+                updateHeight(properties.editor) : properties.editor;
             utils_1.triggerCustomEvent('markdownx.update', properties.parent, [response]);
         };
         xhr.error = function (response) {
@@ -844,4 +845,5 @@ function removeClass(element) {
 }
 exports.removeClass = removeClass;
 
-},{}]},{},[1]);
+},{}]},{},[1])(1)
+});
